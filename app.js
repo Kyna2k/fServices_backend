15ac6cea026887796be30e644b653138a18ea836
Kyna2k <huynobi1809@gmail.com>
2023-10-15 09:13:59 +0700

    change devices

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const route = require('./routes');
+const route = require('./src/routes/index');
 const app = express();
 
 // view engine setup
