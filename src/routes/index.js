359e10106084ded66be970503835631d6c5bda53
Hoangper <huaminhhoang0251@gmail.com>
2023-10-27 13:35:31 +0700

    Revert "Revert "CRUD ROLE ROOM TYPE""
    This reverts commit b26d00e548486083061606cc1bb9f792fcb42e5e.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ const ApiRoute = require("./apis/index");
 const WebLoginRoute = require("./web/aut.route");
 const AuthWeb = require('../middlewares/authencation.middleware.web')
 const UserRoute = require('./web/users.route')
+const RoleRoute = require('./web/roles.route')
+const RoomRoute = require('./web/room.route')
+const TypeRoute = require('./web/typereport.route')
 const route = (app) => {
   
   //Route API
@@ -13,7 +16,9 @@ const route = (app) => {
   //Route Web
   app.use("/login", WebLoginRoute);
   app.use("/users",UserRoute)
-
+  app.use("/roles",RoleRoute);
+  app.use("/rooms",RoomRoute);
+  app.use("/typereports",TypeRoute);
 
   app.get('/error',[AuthWeb],(req,res) => {
     res.render('error/404')
