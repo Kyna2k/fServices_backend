8f335bf8c18bcbe1c40a66f3d0ae7d4e0c70b410
Huy Phan <97116363+Kyna2k@users.noreply.github.com>
2023-12-25 15:24:32 +0700

    Update cloudinary.js

--- src/common/cloudinary.js
+++ src/common/cloudinary.js
@@ -1,8 +1,6 @@
 const cloudinary = require('cloudinary').v2;
 // Configuration 
 cloudinary.config({
-    cloud_name: "dptqmou33",
-    api_key: "363347973346831",
-    api_secret: "Wlj-qgZs2RSZT029z3Enc8svx-M"
+    
   });
 module.exports = cloudinary;
