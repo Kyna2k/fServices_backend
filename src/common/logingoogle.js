793992c4ecbdf16c5452c23cebd4286fc143fb87
Huy Phan <97116363+Kyna2k@users.noreply.github.com>
2023-12-25 15:25:44 +0700

    Update logingoogle.js

--- src/common/logingoogle.js
+++ src/common/logingoogle.js
@@ -1,7 +1,7 @@
 const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 const GOOGLE_CLIENT_ID =
-  "1083679158518-hujukb7dd3hvpcj4ro6l1hr5nhqtekgk.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
+  "";
+const GOOGLE_CLIENT_SECRET = "";
 const passport = require("passport");
 passport.serializeUser(function (user, done) {
     done(null, user);
